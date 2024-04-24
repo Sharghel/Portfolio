2946d67d29ab02c037f4908ef2a09c02f90c190a
Sharghel <serge.harghel@gmail.com>
2024-04-24 13:45:48 +0200

    :sparkles:| adding veille.php to my site

--- veille.php
+++ veille.php
@@ -53,20 +53,29 @@
             <div class="row">
                <div class="col-lg-12 text_align_center">
                   <div class="full heading_s1">
-                     <h2>A Propos de moi</h2>
+                     <h2>Mon outil de veille :</h2>
                   </div>
                   <div class="full">
-                     <p class="large">Bonjour, je m’appelle Serge Harghel, j’ai 19 ans et je suis actuellement en 2ème année de BTS SIO 
-                        (Services Informatiques aux Organisations) option SLAM (Solutions Logicielles et Application Métier) 
-                        au sein de MyDigitalSchool. Effectuant mon cursus en alternance, je suis également apprenti administrateur 
-                        de données au sein de l’entreprise Rte.</p>
+                     <p class="large">
+                        Lors de ma formation, notre intervenant a choisi de faire en sorte qu'un de nos projets soit un agrégateur de flux RSS, afin que cela constitue à la 
+                        fois un projet et un outil de veille technologique. Voici donc le lien vers l'outil de <a class="lien" style="color: blue;" href="https://veille.sergeharghel.fr">veille Laraveille</a>.
+                     </p>
+                     <p class="large">
+                        Cet outil fonctionne grâce à des flux RSS que l'on doit fournir pour obtenir ensuite tous les liens présents sur notre site.
+                     </p>
                   </div>
                </div>
-               <!-- <div class="col-md-12">
-                  <div class="about_img margin_top_30  text_align_center">
-                     <img src="images/ab_img.png" alt="#" />
-                  </div>
-               </div> -->
+            </div>
+         </div>
+         <div class="col-lg-12 text_align_center">
+            <div class="full heading_s1">
+               <h2>Sujet de veille :</h2>
+            </div>
+            <div class="full">
+               <p class="large">
+               Pour ma veille technologique, j'ai choisi de traiter de l'arrivée de Python au sein d'Excel. <br>
+               Cette innovation, selon moi, permettra une simplification des méthodes et rendra Excel plus à jour technologiquement.
+               </p>
             </div>
          </div>
       </section>
@@ -78,19 +87,29 @@
             <div class="row">
                <div class="col-lg-12 text_align_center">
                   <div class="full heading_s1">
-                     <h2>Réalisations professionnel</h2>
+                     <h2>Source</h2>
                   </div>
                   <div class="full">
                      <p class="large">
-                        Concernant mes réalisations professionnel vous trouverez sur ce site les différents projets que j'ai abordé durant mes 2 années de BTS SIO, et si dessous mon CV
+                        Grâce à mon outil Laraveille, j'ai trouvé un article qui parle de cette nouveauté.
                      </p>
+                     <a class="blue_bt" href="https://www.01net.com/actualites/les-bonnes-choses-finissent-toujours-par-arriver-comme-python-dans-excel.html" target="_blank"> Voir la source</a>
                   </div>
                </div>
             </div>
+            <br><br><br><br>
             <div class="row">
                <div class="col-md-12">
-                  <div class="full center">
-                      <a class="blue_bt" href="images/CV.pdf" target="_blank"> Voir mon CV</a>
+                  <div class="full">
+                     <p class="large">
+                        Une fois que j'ai eu cette source, cela m'a permis de me poser davantage de questions sur le sujet et d'approfondir mes connaissances pour en savoir plus.
+                        En explorant le sujet de manière approfondie, j'ai trouvé d'autres sources, ce qui m'a permis non seulement d'élargir mes références dans mon outil de veille, mais aussi d'approfondir mes connaissances sur le sujet.
+                     </p>
+                     <p class="large">
+                        Voici les différents liens qui m'ont permis d'en apprendre plus sur le sujet :
+                     </p>
+                     <a class="blue_bt" href="https://www.zdnet.fr/actualites/python-arrive-dans-microsoft-excel-39960958.htm" target="_blank"> ZDnet.fr <a>
+                     <a class="blue_bt" href="https://www.informatiquenews.fr/python-arrive-dans-excel-mais-ca-ne-restera-pas-gratuit-longtemps-94804" target="_blank"> InformatiqueNews.fr </a>
                   </div>
                </div>
             </div>
