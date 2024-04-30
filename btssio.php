c4ea12f2b3e34374b70708987450cb461212cd90
Sharghel <serge.harghel@gmail.com>
2024-04-30 09:06:47 +0200

    :memo:| change page bts sio

--- btssio.php
+++ btssio.php
@@ -66,13 +66,12 @@
                      équipements d’interconnexion. <br><br>
                      Voici quelques exemples de métiers que les étudiants peuvent exercer après le BTS SIO option SLAM :</p>
                      <ul class="listing-ul">
-                        <li class="listing-li">- Technicien réseau — télécoms</li>
-                        <li class="listing-li">- Technicien systèmes et réseaux</li>
-                        <li class="listing-li">- Administrateur systèmes et réseaux</li>
-                        <li class="listing-li">- Support systèmes et réseaux</li>
-                        <li class="listing-li">- Technicien d’infrastructure</li>
-                        <li class="listing-li">- Pilote d’exploitation</li>
-                        <li class="listing-li">- Technicien de production</li>
+                        <li class="listing-li">- Développeur d’applications informatiques</li>
+                        <li class="listing-li">- Chargé d’études informatiques</li>
+                        <li class="listing-li">- Responsable des services applicatifs</li>
+                        <li class="listing-li">- Programmeur analyste</li>
+                        <li class="listing-li">- Programmeur d’applications</li>
+                        <li class="listing-li">- Technicien d’études informatiques</li>
                      </ul>
                </div>
 
@@ -83,20 +82,21 @@
                  </div>
                  <h3>Option SISR</h3>
                  <p>L’acronyme SISR signifie « Solutions d’infrastructure, systèmes et réseaux ». <br><br>
-                  L’option SISR est donc d’abord destinée aux étudiants qui s’orientent vers les métiers liés à la conception 
-                  et la maintenance d’infrastructures réseaux. Avant tout cela correspond à assurer la sécurité, la maintenance et 
-                  l’installation des réseaux et des équipements informatiques font partie des principales missions des futurs 
-                  administrateurs, techniciens ou pilotes d’exploitation.
-                  <br><br><br><br>
-                  Voici quelques exemples de métiers que les étudiants peuvent exercer après le BTS SIO option SISR :</p>
-                  <ul class="listing-ul">
-                     <li class="listing-li">- Développeur d’applications informatiques</li>
-                     <li class="listing-li">- Chargé d’études informatiques</li>
-                     <li class="listing-li">- Responsable des services applicatifs</li>
-                     <li class="listing-li">- Programmeur analyste</li>
-                     <li class="listing-li">- Programmeur d’applications</li>
-                     <li class="listing-li">- Technicien d’études informatiques</li>
-                  </ul>
+                 L’option SISR est donc d’abord destinée aux étudiants qui s’orientent vers les métiers liés à la conception 
+                 et la maintenance d’infrastructures réseaux. Avant tout cela correspond à assurer la sécurité, la maintenance et 
+                 l’installation des réseaux et des équipements informatiques font partie des principales missions des futurs 
+                 administrateurs, techniciens ou pilotes d’exploitation.
+                 <br><br><br><br>
+                 Voici quelques exemples de métiers que les étudiants peuvent exercer après le BTS SIO option SISR :</p>
+                 <ul class="listing-ul">
+                    <li class="listing-li">- Technicien réseau — télécoms</li>
+                    <li class="listing-li">- Technicien systèmes et réseaux</li>
+                    <li class="listing-li">- Administrateur systèmes et réseaux</li>
+                    <li class="listing-li">- Support systèmes et réseaux</li>
+                    <li class="listing-li">- Technicien d’infrastructure</li>
+                    <li class="listing-li">- Pilote d’exploitation</li>
+                    <li class="listing-li">- Technicien de production</li>
+                 </ul>
               </div> 
             </div>
          </div>
@@ -104,7 +104,7 @@
       <!-- end section -->
      
       <?php require_once('include/footer.php')?>
-      <!-- Core JavaScript
+      <!-- Coreq JavaScript
          ================================================== -->
       <script src="js/jquery.min.js"></script>
       <script src="js/tether.min.js"></script>
